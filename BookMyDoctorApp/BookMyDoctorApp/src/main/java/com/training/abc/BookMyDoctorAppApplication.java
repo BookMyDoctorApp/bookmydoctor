dbc5cfc554ffeece2bd1da5b4e34aa696b7ee8b2
BookMyDoctorApp <123922943+BookMyDoctorApp@users.noreply.github.com>
2023-01-30 16:07:27 +0530

    Add files via upload

---
+++ BookMyDoctorApp/BookMyDoctorApp/src/main/java/com/training/abc/BookMyDoctorAppApplication.java
@@ -0,0 +1,13 @@
+package com.training.abc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMyDoctorAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookMyDoctorAppApplication.class, args);
+	}
+
+}
