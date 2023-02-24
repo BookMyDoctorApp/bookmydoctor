774cf8a5dab458d69bbee72809ea9684e1ff4f23
BookMyDoctorApp <123922943+BookMyDoctorApp@users.noreply.github.com>
2023-02-25 00:59:07 +0530

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,40 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule, Routes } from '@angular/router';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AddDoctorComponent } from './add-doctor/add-doctor.component';
-import { ViewPatientComponent } from './view-patient/view-patient.component';
-import { ViewAppointmentComponent } from './view-appointment/view-appointment.component';
-import { ViewFeedbacksComponent } from './view-feedbacks/view-feedbacks.component';
-import { ViewDoctorsComponent } from './view-doctors/view-doctors.component';
-import { AddHospitalComponent } from './add-hospital/add-hospital.component';
-import { ViewHospitalComponent } from './view-hospital/view-hospital.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HomePageComponent } from './home-page/home-page.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { HelpComponent } from './help/help.component';
+import { RouterModule, Routes } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-import { ApolloComponent } from './apollo/apollo.component';
-const allLinks:Routes=[
-  {path:'addHospital',component:AddHospitalComponent},
-  {path:'addDoctor',component:AddDoctorComponent},
-  {path:'viewHospital',component:ViewHospitalComponent},
-  {path:'viewDoctors',component:ViewDoctorsComponent},
-  {path:'viewPatient',component:ViewPatientComponent},
-  {path:'viewAppointment',component:ViewAppointmentComponent},
-  {path:'viewFeedbacks',component:ViewFeedbacksComponent},
-  {path:'search',component:ApolloComponent}
+import { HttpClientModule } from '@angular/common/http';
+const allLinks:Routes = [
+  {path:'aboutUs',component:AboutUsComponent},
+  {path:'homePage',component:HomePageComponent},
+  {path:'aboutUs',component:AboutUsComponent},
+  {path:'help',component:HelpComponent},
+
 ]
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddDoctorComponent,
-    ViewPatientComponent,
-    ViewAppointmentComponent,
-    ViewFeedbacksComponent,
-    ViewDoctorsComponent,
-    AddHospitalComponent,
-    ViewHospitalComponent,
-    ApolloComponent
+    HomePageComponent,
+    AboutUsComponent,
+    HelpComponent
   ],
   imports: [
     BrowserModule,
@@ -42,10 +30,8 @@ const allLinks:Routes=[
     RouterModule.forRoot(allLinks),
     HttpClientModule,
     FormsModule
-    
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
-
